911f4093dd9d7e440d8be56bc96ddb01a4604ee7
godfreykimani <kimanigodfrey4@gmail.com>
2018-06-07 00:02:42 +0300

    Added message string

--- app/src/main/java/com/example/godfreykimani/justjava/MainActivity.java
+++ app/src/main/java/com/example/godfreykimani/justjava/MainActivity.java
@@ -35,7 +35,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(10 * quantity);
+//        displayPrice(10 * quantity);
+
+        String priceMessage = "Free";
+        displayMessage(priceMessage);
     }
 
     /*Set increment on clicking the + button*/
@@ -66,6 +69,14 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 }
